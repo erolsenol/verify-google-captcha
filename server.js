9caf4b5a61d0b4d80b7e07165de3fff60146ccef
erolsenol <erol.senol@outlook.com>
2022-01-30 19:35:36 +0300

    dev-env-added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const https = require("https");
+const http = require("http");
 const fs = require("fs");
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const sendgrid = require("@sendgrid/mail");
@@ -9,28 +10,34 @@ dotenv.config();
 
 sendgrid.setApiKey(process.env.SG_API_KEY);
 
-const emailHandler = require('./email-handler');
+const emailHandler = require("./email-handler");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/', emailHandler);
+app.use("/", emailHandler);
 
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   const sslOptions = {
-    pfx: fs.readFileSync('./ssl/last.pfx'),
+    pfx: fs.readFileSync("./ssl/last.pfx"),
     passphrase: process.env.PASSPHRASE,
   };
-  
+
   const httpsServer = https.createServer(sslOptions, app);
-  
-  const port = (process.env.PORT || 443);
-  
-  httpsServer.listen(port, () => {
-    console.log(`HTTPS Server running on port ${port}`);
-  });
+  const port = process.env.PORT || 443;
+
+  if (process.env.NODE_ENV === "development") {
+    const httpServer = http.createServer(app);
+    httpServer.listen(port, () => {
+      console.log(`HTTP Server running on port ${port}`);
+    });
+  } else {
+    httpsServer.listen(port, () => {
+      console.log(`HTTPS Server running on port ${port}`);
+    });
+  }
 }
 
 module.exports = app;
